875149a4b0f8c6489e1830d282f82bfb41371b4d
taashev <taashev.a.r92@yandex.ru>
2022-11-07 11:19:58 +0300

    ref: alt img order datails

--- src/components/OrderDetails/Order-details.jsx
+++ src/components/OrderDetails/Order-details.jsx
@@ -7,7 +7,7 @@ export function OrderDetails({ orderId }) {
 		<div className={`pt-4 pb-15 ${styles['order-details']}`}>
 			<div className={`text_type_digits-large mb-8`}>{ orderId }</div>
 			<span className={`text_type_main-medium mb-15`}>идентификатор заказа</span>
-			<img className={`${styles['order-details__img']}`} src={img} alt="" />
+			<img className={`${styles['order-details__img']}`} src={img} alt="заказ готов" />
 			<span className={`mt-15`}>Ваш заказ начали готовить</span>
 			<span className={`mt-2 ${styles['order-details__text']}`}>Дождитесь готовности на орбитальной станции</span>
 		</div>
