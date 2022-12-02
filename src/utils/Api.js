8036784a0677aeb7b651d038452a4feaa1ab454e
taashev <taashev.a.r92@yandex.ru>
2022-12-02 03:11:50 +0300

    feat: api register, reset password

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -9,11 +9,13 @@ function checkResponse(res) {
 		.then((res) => { throw new Error(res) })
 };
 
+// get ingredients
 export function getIngredients() {
 	return fetch(`${BASE_URL}/api/ingredients`)
 		.then(checkResponse)
 };
 
+// set order
 export function setOrder(order) {
 	return fetch(`${BASE_URL}/api/orders`, {
 		method: 'POST',
@@ -26,12 +28,39 @@ export function setOrder(order) {
 	}).then(checkResponse)
 };
 
-export function setResetPassword(email) {
+// sing up
+export function signUp(email, password, name) {
+	return fetch(`${BASE_URL}/api/auth/register`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			email,
+			password,
+			name,
+		}),
+	}).then(checkResponse)
+};
+
+// email reset password
+export function setEmailResetPassword(email) {
 	return fetch(`${BASE_URL}/api/password-reset`, {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
 		},
 		body: JSON.stringify({ email })
+	}).then(checkResponse)
+};
+
+// reset password
+export function setResetPassword(password, token) {
+	return fetch(`${BASE_URL}/api/password-reset/reset`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({ password, token })
 	}).then(checkResponse)
 };
