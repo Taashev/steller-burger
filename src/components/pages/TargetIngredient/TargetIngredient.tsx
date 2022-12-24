20bc162cc1343382ab9654851add48b08808842c
taashev <taashev.a.r92@yandex.ru>
2022-12-24 21:31:54 +0300

    ref: Register: jsx into tsx

---
+++ src/components/pages/TargetIngredient/TargetIngredient.tsx
@@ -0,0 +1,10 @@
+import { IngredientDetails } from '../../Burger-ingredients/IngredientDetails/Ingredient-details';
+
+export function TargetIngredient() {
+  return (
+    <>
+      <h1 style={{ textAlign: 'center' }}>Детали ингредиента</h1>
+      <IngredientDetails />
+    </>
+  );
+}
