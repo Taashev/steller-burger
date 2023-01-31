3c446a7477ca69022d0fdff2021e5d4056c48f3f
taashev <taashev.a.r92@yandex.ru>
2023-01-31 13:58:51 +0300

    Updates

--- src/components/pages/Target-order-details/Target-order-details.tsx
+++
@@ -1,5 +0,0 @@
-import { OrderDetails } from '../../Order-details/Order-details';
-
-export function TargetOrderDetails(): JSX.Element {
-  return <OrderDetails page={true} />;
-}
--- src/services/types/locationState.ts
+++
@@ -1,9 +0,0 @@
-export interface ILocationState {
-  hash: string;
-  key: string | undefined;
-  pathname: string;
-  search: string;
-  state: ILocationState | null;
-  background?: ILocationState | undefined;
-  from?: ILocationState | undefined;
-}
